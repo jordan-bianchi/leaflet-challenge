8d498c83e664055e8c256aac4ccb10d0a9e8c01e
jordan_bianchi <jordan_bianchi@yahoo.com>
2021-07-21 19:13:38 -0400

    fix files

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,16 +1,2 @@
-var myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
-});
-
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-}).addTo(myMap);
-
-var earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-
+// API key
+const API_KEY = "sk.eyJ1Ijoiam9yZGFuYmlhbmNoaSIsImEiOiJja3F2aHhiY3gwZWx4Mm90OTJnMWkweG14In0.DyBYYR48pM2u9cymvdwq3A";
